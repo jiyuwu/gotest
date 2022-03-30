d7f17ff6384de49b25e36f719f35076a9b704943
yincihang <923374829@qq.com>
2022-03-30 10:36:49 +0800

    websocket test

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/jiyuwu/gotest
 
 go 1.16
+
+require (
+	github.com/gin-gonic/gin v1.7.7
+	github.com/gorilla/websocket v1.5.0
+)
--- testweb/main.go
+++ testweb/main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+	"github.com/jiyuwu/gotest/testweb/controllers"
+)
 
 func main() {
 	fmt.Println("1111111")
+
+	//websocket测试
+	router := gin.Default()
+	router.GET("/wx", controllers.WsHandle)
 }
