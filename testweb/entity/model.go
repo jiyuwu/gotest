c642a43c4849d176cefaa0c00d5e46f2d1d6f3d3
yincihang <923374829@qq.com>
2022-03-30 19:15:17 +0800

    json up

--- testweb/entity/model.go
+++ testweb/entity/model.go
@@ -8,6 +8,6 @@ type ModelPrefix struct {
 
 // ModelSuffix 如果只需要创建与修改时间，则用这个
 type ModelSuffix struct {
-	CreatedAt *time.Time `gorm:"column:CreateTime;index"`
-	UpdatedAt *time.Time `gorm:"column:UpdateTime"`
+	CreatedAt *time.Time `json:"createdAt,omitempty" gorm:"column:CreateTime;index"`
+	UpdatedAt *time.Time `json:"updatedAt,omitempty" gorm:"column:UpdateTime"`
 }
