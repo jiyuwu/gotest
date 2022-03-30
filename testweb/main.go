bf6a539b68cbab2bed9a818a9675157f5ab1bdea
yincihang <923374829@qq.com>
2022-03-30 13:57:49 +0800

    ws up

--- testweb/main.go
+++ testweb/main.go
@@ -1,16 +1,23 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/gin-gonic/gin"
 	"github.com/jiyuwu/gotest/testweb/controllers"
+	"github.com/jiyuwu/gotest/testweb/middleware"
 )
 
 func main() {
-	fmt.Println("1111111")
+	gin.SetMode(gin.ReleaseMode) //线上环境
 
+	go controllers.Manager.Start()
 	//websocket测试
-	router := gin.Default()
-	router.GET("/wx", controllers.WsHandle)
+	r := gin.Default()
+	r.Use(middleware.Cors())
+	r.GET("/ws", controllers.WsHandler)
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run(":80")
 }
